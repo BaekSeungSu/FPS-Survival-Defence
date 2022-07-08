b96068ea13b5e0e595af55c70ae43c6adce692d6
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2022-07-09 00:28:03 +0900

    Add Run Crouch Jump

--- FpsSurvivalDefence/Assets/Scripts/PlayerController.cs
+++ FpsSurvivalDefence/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //스피드 조정 변수
     [SerializeField]
     private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    [SerializeField]
+    private float crouchSpeed;
+
+    private float applySpeed;
+
+    [SerializeField]
+    private float jumpForce;
+
+    //상태변수
+    private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = true;
+
+    //앉았을 때 얼마나 앉을지 결정하는 변수
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    private CapsuleCollider capsuleCollider;
 
     //카메라 민감도
     [SerializeField]
     private float lookSensitivity;
 
-    //카메라 각도
+    //카메라 한계 각도
     [SerializeField]
     private float cameraRotationLimit;
     private float currentCameraRotationX = 0;
 
+    //필요한 컴포넌트
     [SerializeField]
     private Camera theCamera;
 
@@ -24,17 +48,26 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
     }
 
     // Update is called once per frame
     void Update()
     {
+        IsGround();
+        TryJump();
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
 
+    //움직임 실행
     private void Move() 
     {
         float _moveDirX = Input.GetAxisRaw("Horizontal");
@@ -43,27 +76,131 @@ public class PlayerController : MonoBehaviour
         Vector3 _moveHorizontal = transform.right * _moveDirX;
         Vector3 _moveVertical = transform.forward * _moveDirZ;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
         //(1,0,0)(0,0,1)
         //(1,0,1) = 2
         // (0.5,0,0.5) = 1   <- normalized 하면 이렇게 바뀜
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
         
     }
+
+    //상하 카메라 회전
     private void CameraRotation()
     {
-        //상하 카메라 회전
         float _xRotation = Input.GetAxisRaw("Mouse Y");
         float _cameraRotationX = _xRotation * lookSensitivity;
         currentCameraRotationX -= _cameraRotationX;
         currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
         theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
     }
+
+    //좌우 캐릭터 회전
     private void CharacterRotation()
     {
-        //좌우 캐릭터 회전
         float _yRotation = Input.GetAxisRaw("Mouse X");
         Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
         myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+    }
+
+    //달리기 시도
+    private void TryRun()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    //달리기 실행
+    private void Running()
+    {
+        //앉은 상태에서 달리기 실행시 달리기 취소
+        if (isCrouch)
+            Crouch();
+        
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    //달리기 취소
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    //점프시도
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    //점프
+    private void Jump()
+    {
+        //앉은 상태에서 점프시 앉은상태 해제
+        if (isCrouch)
+            Crouch();
+
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    //지면 체크
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    //앉기 시도
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    //앉기 함수
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    //부드러운 앉기 동작
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+
+        while(_posY != applyCrouchPosY)
+        {
+            count++;
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.025f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            if (count > 70)
+                break;
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
     }
 }
